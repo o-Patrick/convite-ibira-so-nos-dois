de167e64dcb81eb6dad9cc4a955fc87fa3163700
Patrick <leme.patrick.contato@hotmail.com>
2022-12-18 11:28:03 -0300

    Initial commit

---
+++ confirmacao.php
@@ -0,0 +1,83 @@
+<?php
+  require_once('PHPMailer/src/PHPMailer.php');
+	require_once('PHPMailer/src/SMTP.php');
+	require_once('PHPMailer/src/Exception.php');
+
+  use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+  if(isset($_POST["btnSubmit"])) {
+    if (isset($_POST["nome"])) $nome = $_POST["nome"];
+    if (isset($_POST["email"])) $email = $_POST["email"];
+    if (isset($_POST["mensagem"])) $mensagemDela = $_POST["mensagem"];
+
+    $mail = new PHPMailer(true);
+
+    // e-mail pra ela
+		try {
+			// para depuração
+			// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+			$mail->isSMTP();
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->SMTPSecure = "tls";
+			$mail->Port = 587;
+			$mail->SMTPOptions = array(
+				'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+				)
+			);
+			$mail->Username = 'convite.para.bianca@gmail.com';
+			$mail->Password = 'akiv qmyn pvxf bbrj';
+
+			$mail->setFrom('convite.para.bianca@gmail.com');
+			$mail->addAddress($email);
+
+			$mail->isHTML(true);
+      $mail->CharSet = 'UTF-8';
+			$mail->Subject = 'Presença confirmada!';
+			$mail->Body = "Sua confirmação foi enviada com sucesso! Confira sua mensagem: <hr> $mensagemDela";
+
+			if($mail->send()) {
+        echo "<script> console.log('E-mail de confirmação enviado com sucesso!') </script>";
+      } else {
+				echo "<script> alert('ERRO: resposta não enviada!') </script>";
+			} // if mail send
+		} catch (Exception $e) {
+			echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+		} // try contrução e-mail
+
+    // e-mail pra mim
+		try {
+			// para depuração
+			// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+			$mail->isSMTP();
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->SMTPSecure = "tls";
+			$mail->Port = 587;
+			$mail->Username = 'convite.para.bianca@gmail.com';
+			$mail->Password = 'akiv qmyn pvxf bbrj';
+
+			$mail->setFrom('convite.para.bianca@gmail.com');
+			$mail->addAddress('patrickleme02@gmail.com');
+
+			$mail->isHTML(true);
+			$mail->Subject = "$nome confirmou presença!";
+			$mail->Body = "$nome confirmou presença no Ibirapuera em XX/12! Irra! Mensagem recebida: <hr> $mensagemDela";
+
+			if($mail->send()) {
+        echo "<script> console.log('E-mail para adm enviado com sucesso!') </script>";
+      } else {
+				echo "<script> alert('ERRO: resposta não enviada!') </script>";
+			} // if mail send
+		} catch (Exception $e) {
+			echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+		} // try contrução e-mail
+		echo "<script>alert('Presença confirmada com sucesso!')</script>";
+    echo "<meta http-equiv='refresh' content='0; index.html'>";
+  } // if isset btnSubmit
+?>
